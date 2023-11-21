5351c11934294e624d09e509fa4086fdb31160c3
hiro502 <hiro1887v@gmail.com>
2023-11-22 00:29:18 +0100

    fonction de suppression de travaux existants

--- FrontEnd/assets/main.js
+++ FrontEnd/assets/main.js
@@ -1,100 +1,197 @@
+'use strict';
+
 import {addElementToIndex} from "./common.js"
 
-// Sélectionner le conteneur HTML avec la classe 'gallery'
-const container = document.querySelector('.gallery');
-let projetsData;
+// Récupérer les données des projets depuis le stockage local s'ils existent, sinon les récupérer depuis l'API
+let projetsData = window.localStorage.getItem('projetsData');
+
+if(projetsData === null){
+    fetch('http://localhost:5678/api/works')
+    .then(response => response.json())
+    .then(data => {
+      projetsData = data;
+      const valeurProjetsData = JSON.stringify(projetsData);
+      window.localStorage.setItem("projetsData", valeurProjetsData);
+  })
+  .catch(error => console.error('Erreur lors de la récupération des pièces depuis l\'API:', error));
+} else {
+    projetsData = JSON.parse(projetsData);
+}
+
 
 // Fonction pour générer et ajouter un projet au conteneur
-function genererProjet(projet) {
-  const figure = document.createElement('figure');
-  const img = document.createElement('img');
-  img.src = projet.imageUrl;
-  img.alt = projet.title;
-  const figcaption = document.createElement('figcaption');
-  figcaption.textContent = projet.title;
+const containerGalley = document.querySelector('.gallery');
 
-  figure.appendChild(img);
-  figure.appendChild(figcaption);
 
-  container.appendChild(figure);
-}
-
-// Fonction pour récupérer les données des projets depuis l'API
-const fetchProjetsData = async () => {
-  try {
-    const response = await fetch('http://localhost:5678/api/works');
-    projetsData = await response.json();
-  } catch (error) {
-    console.error('Erreur lors de la récupération des projets depuis l\'API:', error.message);
-  }
-}
-
-// Fonction pour afficher les projets filtrés dans le conteneur
-const afficherProjets = (projetsFiltres) => {
-  container.innerHTML = ''; // Vider le contenu du conteneur
-
-  projetsFiltres.forEach(projet => {
-    genererProjet(projet);
-  });
-}
-
-// Fonction d'initialisation, appelée au chargement de la page
-const filtre = async () => {
-    await fetchProjetsData();
-    afficherProjets(projetsData); // Afficher tous les projets
+function genererProjet (projetsData)  {
+    
   
-    // Ajouter des écouteurs d'événements aux boutons
+    for (let i = 0; i < projetsData.length; i++) {
+    
+    const projetElement = projetsData[i];
+    
+    const figure = document.createElement('figure');
+    const img = document.createElement('img');
+    const figcaption = document.createElement('figcaption');
+    figure.id = projetElement.id;
+    
+    img.src = projetElement.imageUrl;
+    img.alt = projetElement.title;
+   figcaption.textContent = projetElement.title;
+  
+    figure.appendChild(img);
+    figure.appendChild(figcaption);
+    containerGalley.appendChild(figure);
+  }
+ 
+}
+
+genererProjet(projetsData);
+
+ 
+
     const btnTous = document.querySelector('.btn-tous');
     btnTous.addEventListener('click', () => {
-      const projetsTous = projetsData;
-      afficherProjets(projetsTous);
+      containerGalley.innerHTML = '';
+      genererProjet(projetsData);
     });
-
+    
     const btnObjets = document.querySelector('.btn-objets');
     btnObjets.addEventListener('click', () => {
       const projetObjets = projetsData.filter(projet => projet.categoryId == 1);
-      afficherProjets(projetObjets);
+      console.log(projetObjets);
+      containerGalley.innerHTML = '';
+      genererProjet(projetObjets);
     });
-  
+
     const btnAppartement = document.querySelector('.btn-appartement');
     btnAppartement.addEventListener('click', () => {
       const projetAppartement = projetsData.filter(projet => projet.categoryId == 2);
-      afficherProjets(projetAppartement);
+      containerGalley.innerHTML = '';
+      genererProjet(projetAppartement);
     });
 
-    const btnHotel = document.querySelector('.btn-hotel');
-    btnHotel.addEventListener('click', () => {
+const btnHotel = document.querySelector('.btn-hotel');
+btnHotel.addEventListener('click', () => {
       const projetHotel = projetsData.filter(projet => projet.categoryId == 3);
-      afficherProjets(projetHotel);
+      containerGalley.innerHTML = '';
+      genererProjet(projetHotel);
     });
-  }
-
-// Appeler la fonction d'initialisation
-filtre();
 
 
-
-
-//  document.addEventListener('DOMContentLoaded', function() {
-//       addElementToIndex();
-//     });
 
 function addElementEdition(){
     const token = localStorage.getItem('accessToken');
     if (token) {
         addElementToIndex();
-        
         const btnLogout = document.getElementById('btnLogout');
-
         btnLogout.addEventListener('click', function(event) {
             localStorage.removeItem('accessToken');
             console.log('User logged out.');
-            event.target.textContent = 'Login';
+            event.target.textContent = 'login';
             event.target.className = 'btnLogin';
         });
-
+        const filtre = document.querySelector('.filtres');
+        filtre.innerHTML = '';
     }
 }
 
 addElementEdition();  
+
+
+const containerGalleyModal = document.querySelector('.gallery-modal');
+
+function genererGalleyModal(projetsData) {
+  for (let i = 0; i < projetsData.length; i++) {
+  
+  const projetElement = projetsData[i];
+
+
+
+  const figure = document.createElement('figure');
+  const img = document.createElement('img');
+  const div = document.createElement('div');
+  figure.className ='projet-modal';
+  div.className ='remove-projet';
+  img.src = projetElement.imageUrl;
+  img.alt = projetElement.title;
+  div.innerHTML = '<i class="fa-solid fa-trash-can fa-xs" style="color: #ffffff;"></i>';
+  div.id = projetElement.id;
+  div.addEventListener('click', function() {
+    handleClick(this);
+  });
+ 
+
+  figure.appendChild(img);
+  figure.appendChild(div);
+  containerGalleyModal.appendChild(figure);
+}}
+
+genererGalleyModal(projetsData);
+
+function fetchDataAndRender() {
+  fetch('http://localhost:5678/api/works')
+    .then(response => response.json())
+    .then(data => {
+      projetsData = data;
+      const valeurProjetsData = JSON.stringify(projetsData);
+      window.localStorage.setItem("projetsData", valeurProjetsData);
+      containerGalley.innerHTML = '';
+      containerGalleyModal.innerHTML ='';
+      genererProjet(projetsData);
+      genererGalleyModal(projetsData);
+    })
+    .catch(error => console.error('Erreur lors de la récupération des pièces depuis l\'API:', error));
+}
+
+
+function handleClick(clickedElement) {
+  const elementId = clickedElement.id;
+  const token = localStorage.getItem('accessToken');
+  console.log(elementId);
+  if (elementId) {
+    const confirmation = confirm(`Voulez-vous supprimer ce projet ?`);
+
+    if (confirmation) {
+      // L'utilisateur a cliqué sur OK. Poursuite de l'action...
+      console.log("L'utilisateur a cliqué sur OK. Poursuite de l'action...");
+
+      // Envoi de la requête DELETE
+      fetch(`http://localhost:5678/api/works/${elementId}`, {
+        method: 'DELETE',
+        headers: {
+          'Content-Type':'application/json',
+          Authorization: `Bearer ${token}`,
+        }
+      })
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('La réponse du réseau n\'était pas valide');
+        }
+        console.log('Requête DELETE réussie');
+        // Traitement supplémentaire ici
+        fetchDataAndRender();
+      }).catch(error => {
+        console.error('Problème avec la requête DELETE :', error);
+      });
+    } else {
+      // L'utilisateur a cliqué sur Annuler. Action annulée.
+      console.log("L'utilisateur a cliqué sur Annuler. Action annulée.");
+    }
+  }
+}
+
+
+const openModal = document.getElementById('openModalBtn');
+const closeModal = document.getElementById('closeModalBtn') 
+const modal = document.getElementById('modalEdition');
+
+openModal.addEventListener('click', () => {
+  modal.style.display = 'block';
+})
+
+
+closeModal.addEventListener('click', () => {
+  modal.style.display = 'none';
+})
 
